ff5e607f21bf9ea8acaf08d8a8fcb91b2a4dbc55
Yosevu Kilonzo <yosevuk@gmail.com>
2020-12-26 21:45:59 -0500

    solve(javascript): Exercise 3.2 - makeMonitored

--- javascript/exercise-3-1.js
+++ javascript/exercise-3-1.js
@@ -13,3 +13,29 @@ equal(a(10), 15)
 equal(a(10), 25)
 equal(a(10), 35)
 
+// makeMonitored
+const makeMonitored = f => {
+  let count = 0
+
+  return function mf(input) {
+
+    if (input === 'how many calls') {
+      return count
+    }
+
+    if (input === 'reset count') {
+      count = 0
+      return count
+    }
+
+    count = count + 1
+    return f(input)
+  }
+}
+
+const s = makeMonitored(Math.sqrt)
+
+equal(s(100), 10)
+equal(s('how many calls'), 1)
+equal(s('reset count'), 0)
+
